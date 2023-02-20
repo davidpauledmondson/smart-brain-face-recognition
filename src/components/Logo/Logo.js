3e40a1bc5c8b5efd63db9d1195dceb64866e2e0a
davidedmondson <dpe107@mail.com>
2023-02-19 21:14:08 -0600

    minor changes

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,7 +6,7 @@ import './Logo.css';
 const Logo = () => {
   return (
     <div className='ma4 mt0'>
-      <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+      <Tilt glareEnable="true" className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
         <div className="Tilt-inner pa3">
           <img style={{paddingTop: '5px'}} alt='logo' src={brain}/>
         </div>
@@ -14,16 +14,5 @@ const Logo = () => {
     </div>
   );
 }
-
-// SHOULD IT BE THE BELOW LINE?
-// const Logo = () => {
-//   return (
-//       <div className="ma4 mt0">
-//           <Tilt glareEnable='true' className="Tilt br2 shadow-2">
-//               <img src={ brain } alt="brain logo"/>
-//           </Tilt>
-//       </div>
-//   )
-// }
 
 export default Logo;
