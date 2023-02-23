5ab3f2eeea2b0a70bca480cb8d98ccaaa456ff1b
davidedmondson <dpe107@mail.com>
2023-02-23 15:36:17 -0600

    connecting front-end to remote server

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch('https://spring-paper-2518.fly.dev/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
